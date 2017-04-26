ac8e958839a2cdd4b3eddb256990baede5831208
Vu Dang <jublusion5@gmail.com>
2017-04-26 17:27:13 -0500

    Created Place Object

---
+++ SfaraProject/Place.swift
@@ -0,0 +1,22 @@
+//
+//  Place.swift
+//  SfaraProject
+//
+//  Created by Vu Dang on 4/26/17.
+//  Copyright © 2017 Vu Dang. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+    fileprivate var location: String?
+    fileprivate var temperature: Double?
+    fileprivate var forecast: String?
+    fileprivate var time: String?
+    
+    init() {
+        
+    }
+    
+}
