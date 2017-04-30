4e43d9288e8afdaa752eaf2cddfaaf323fe82153
Vu Dang <jublusion5@gmail.com>
2017-04-30 11:10:41 -0500

    Started created weatherDisplayView at the top and applied constraints

---
+++ SfaraProject/WeatherDisplayView.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDisplayView.swift
+//  SfaraProject
+//
+//  Created by Vu Dang on 4/30/17.
+//  Copyright © 2017 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDisplayView: UIView {
+
+    override func awakeFromNib() {
+        backgroundColor = UIColor(white: 0.75, alpha: 0.6)
+    }
+
+}
